e17aad84b29843d36bf3f6a70b15cbbb434dc4b2
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-08-01 18:07:55 +0200

    initialize the project

---
+++ src/main/java/com/example/artifact_upload_demo/ArtifactUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.artifact_upload_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactUploadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactUploadDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artifact_upload_demo/ArtifactUploadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artifact_upload_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactUploadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
