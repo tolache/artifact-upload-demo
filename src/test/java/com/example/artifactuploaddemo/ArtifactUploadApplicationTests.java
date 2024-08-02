75b0f3dbce8f7792c3af1bbdf6e42394e785bb40
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2024-08-02 13:34:28 +0200

    Fix error with failing build due to incomplete @SpringBootTest annotation

--- src/test/java/com/example/artifactuploaddemo/ArtifactUploadApplicationTests.java
+++ src/test/java/com/example/artifactuploaddemo/ArtifactUploadApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.artifactuploaddemo;
 
+import com.example.artifactuploaddemo.server.ArtifactUploadApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ArtifactUploadApplication.class)
 class ArtifactUploadApplicationTests {
 
 	@Test
